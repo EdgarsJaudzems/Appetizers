6f7155cea64444043b60dbf50826a70f3caffcbc
Edgars Jaudzems <73849363+EdgarsJaudzems@users.noreply.github.com>
2023-10-03 10:52:42 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by edgars.jaudzems on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
